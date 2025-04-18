43cc54a5893bf5905b3c50f642b6925e1cb356c8
Javier Gonzalez <javier.e.glz@gmail.com>
2025-04-18 09:31:56 -0500

    feat: Add story for index page

---
+++ apps/admin-panel/app/ledger-account/ledger-account-page.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import LedgerAccountIndexPage from "./page"
+
+const LedgerAccountIndexPageStory = () => {
+  return <LedgerAccountIndexPage />
+}
+
+const meta: Meta = {
+  title: "Pages/LedgerAccounts",
+  component: LedgerAccountIndexPageStory,
+  parameters: { layout: "fullscreen", nextjs: { appDirectory: true } },
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Index: Story = {
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: `/ledger-account`,
+      },
+    },
+  },
+}
+
